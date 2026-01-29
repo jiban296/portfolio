4ad54059ae7a5a1fa859f1bcf7b76c6bf2ea8cb6
Jiban Adhikari <jibanadhikari296@gmail.com>
2026-01-29 08:46:10 +0545

    update design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,30 @@ import profile from "../assets/images/mypic.jpg";
 function Header(){
     return (
     
-   <div className="min-h-90vh mx-auto px-8 py-4 flex justify-between items-center shadow-2xl">
-      <img src={profile} alt="logo" className="h-24 width-auto object-contain mx-20 rounded-full "/>
-       
-       <div>
-        <ul className="flex items-center justify-center gap-8 text-grey text-2xl font-semibold  ">
+   <header className="w-full shadow-2xl bg-white dark:bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-24">
+        {/* Logo */}
+        <div className="flex items-center">
+          <img
+            src={profile}
+            alt="logo"
+            className="h-24 w-24 object-cover rounded-full"
+          />
+        </div>
+
+       <nav>
+        <ul className="hidden md:flex space-x-8 text-gray-700 dark:text-gray-200 font-semibold text-lg ">
             <li><a href="#home" className=" hover:text-blue-500  hover:bg-blue-500 hover:text-white py-2 px-2  hover:border-2 hover:rounded-md transition">Home</a></li>
-            <li><a href="#about" className="hover:text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-2  hover:border-2 hover:rounded-md transition">About</a></li>
+            <li><a href="#about" className="hover:text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-2  hover:border-2 hover:rounded-md transition">Abouts</a></li>
             <li><a href="#Projects" className="hover:text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-2  hover:border-2 hover:rounded-md transition">MyProjects</a></li>
             <li><a href="#skills" className="hover:text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-2  hover:border-2 hover:rounded-md transition">Skills</a></li>
             <li><a href="#contact" className="hover:text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-2  hover:border-2 hover:rounded-md transition">Contact</a></li>
         </ul>
 
-       </div>
+       </nav>
 
     </div>
+    </header>
     );
 };
 
